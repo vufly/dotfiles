86ae94d71aaaa0d8d983826ec8d5ea66783f36dc
Nguyen Dinh Vu <vu.nguyen@crossian.com>
2021-04-24 11:26:21 +0700

    backup brew

--- nvim/link.sh
+++ nvim/link.sh
@@ -2,3 +2,4 @@ ln -s ~/Tweaking/dotfiles/nvim/init.lua ~/.config/nvim/init.lua
 ln -s ~/Tweaking/dotfiles/nvim/lua ~/.config/nvim/lua
 
 ln -s ~/Tweaking/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
+ln -s ~/Tweaking/dotfiles/nvim/coc-settings.json ~/.config/nvim/coc-settings.json
