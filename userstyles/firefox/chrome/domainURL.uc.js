0ad073474f4eacf6bbd01360df41125c128ec259
Vu Nguyen <divn@outpost24.com>
2022-03-14 13:37:29 +0700

    Update Firefox style
    Some new Firefox style

--- userstyles/firefox/chrome/domainURL.uc.js
+++ userstyles/firefox/chrome/domainURL.uc.js
@@ -10,8 +10,6 @@
 // @onlyonce
 // ==/UserScript==
 
-// based on https://addons.mozilla.org/en-US/firefox/addon/open-link-in-silent-tab/
-
 UC.domainURL = {
   doc: null,
   contentDoc: null,
@@ -20,7 +18,8 @@ UC.domainURL = {
   urlChangeEvent: null,
   observer: null,
 
-  shortenURL: function () {
+  shortenURL: function (e) {
+    // console.log(e?.type);
     try {
       const url = new URL(this.urlBarInput.value);
       if (/(http|https):/.test(url.protocol)) {
@@ -30,25 +29,8 @@ UC.domainURL = {
         this.urlBarInput.value = url.host;
       }
     } catch (err) {
-      console.log(err);
+      // console.log(err);
     }
-  },
-
-  onTabChange: function () {
-    const _this = this
-    setTimeout(() => {
-      try {
-        const url = new URL(_this.urlBarInput.value);
-        if (/(http|https):/.test(url.protocol)) {
-          if (url.host.startsWith("www")) {
-            url.host = url.host.slice(4);
-          }
-          _this.urlBarInput.value = url.host;
-        }
-      } catch (err) {
-        console.log(err);
-      }
-    }, 100)
   },
 
   exec: function (win) {
@@ -61,59 +43,15 @@ UC.domainURL = {
     this.contentDoc = contentDocument;
     this.urlBarInput = document.getElementById('urlbar-input');
     this.urlMouseEvent = this.urlBarInput.addEventListener('blur', () => this.shortenURL());
-    this.urlMouseEvent = this.urlBarInput.addEventListener('change', () => {console.log('change')});
-    gBrowser.tabContainer.addEventListener('TabSelect', () => this.onTabChange());
+    gBrowser.tabContainer.addEventListener('TabSelect', (e) => this.shortenURL(e), false);
+    gBrowser.tabContainer.addEventListener("TabAttrModified", (e) => this.shortenURL(e), false);
+    gBrowser.tabContainer.addEventListener('TabOpen', (e) => this.shortenURL(e), false);
+    gBrowser.tabContainer.addEventListener('TabClose', (e) => this.shortenURL(e), false);
+    gBrowser.tabContainer.addEventListener('load', (e) => this.shortenURL(e), false);
+    gBrowser.tabContainer.addEventListener("DOMContentLoaded", (e) => this.shortenURL(e), false);
   },
 
   destroy: function () {
-    // this.doc.removeEventListener('blur', this.shortenURL);
     delete UC.domainURL;
   }
 }
-
-// UC.domainURL = {
-//   exec: function (win) {
-//     let {
-//       BrowserUIUtils,
-//       XULBrowserWindow
-//     } = win;
-
-//     BrowserUIUtils.trimURL = function trimURL(aURL) {
-//       let urlString = this.removeSingleTrailingSlashFromURL(aURL);
-//       try {
-//         const url = new URL(urlString);
-//         if (/(http|https):/.test(url.protocol)) {
-//           if (url.host.startsWith("www")) {
-//             url.host = url.host.slice(4);
-//           }
-//           return url.host;
-//         } else {
-//           return urlString;
-//         }
-//       } catch (err) {
-//         return urlString;
-//       }
-//     }
-
-//     XULBrowserWindow.setOverLink = function setOverLink(url) {
-//       if (url) {
-//         url = Services.textToSubURI.unEscapeURIForUI(url);
-    
-//         // Encode bidirectional formatting characters.
-//         // (RFC 3987 sections 3.2 and 4.1 paragraph 6)
-//         url = url.replace(
-//           /[\u200e\u200f\u202a\u202b\u202c\u202d\u202e]/g,
-//           encodeURIComponent
-//         );
-//       }
-    
-//       this.overLink = url;
-//       LinkTargetDisplay.update();
-//     }
-//   },
-
-//   destroy: function () {
-//     // this.doc.removeEventListener('blur', this.shortenURL);
-//     delete UC.domainURL;
-//   }
-// }
