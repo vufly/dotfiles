b8c86de9aafa88b0999309c3a526ac37604a29ca
Vu Nguyen <vund.vn@gmail.com>
2022-05-31 18:06:41 +0700

    Style for vertical tab

--- userstyles/firefox/chrome/domainURL.uc.js
+++ userstyles/firefox/chrome/domainURL.uc.js
@@ -1,57 +1,36 @@
 // ==UserScript==
-// @name            Domain URL
-// @author          vufly
-// @include         main
-// @include         chrome://browser/content/places/bookmarksSidebar.xhtml
-// @include         chrome://browser/content/places/historySidebar.xhtml
-// @include         chrome://browser/content/places/places.xhtml
-// @startup         UC.domainURL.exec(win);
-// @shutdown        UC.domainURL.destroy();
-// @onlyonce
+// @name           Domain URL
+// @version        1.0.0
+// @author         vufly
+// @description    Display only Domain in URL bar input
 // ==/UserScript==
+(function () {
 
-UC.domainURL = {
-  doc: null,
-  contentDoc: null,
-  urlBarInput: null,
-  urlMouseEvent: null,
-  urlChangeEvent: null,
-  observer: null,
+  try {
 
-  shortenURL: function (e) {
-    // console.log(e?.type);
-    try {
-      const url = new URL(this.urlBarInput.value);
-      if (/(http|https):/.test(url.protocol)) {
-        if (url.host.startsWith("www")) {
-          url.host = url.host.slice(4);
+    function shortenURL(e) {
+      try {
+        const url = new URL(urlBarInput.value);
+        if (/(http|https):/.test(url.protocol)) {
+          if (url.host.startsWith("www")) {
+            url.host = url.host.slice(4);
+          }
+          urlBarInput.value = url.host;
         }
-        this.urlBarInput.value = url.host;
+      } catch (err) {
+        // console.log(err);
       }
-    } catch (err) {
-      // console.log(err);
     }
-  },
 
-  exec: function (win) {
-    let {
-      document,
-      contentDocument,
-      gBrowser
-    } = win;
-    this.doc = document;
-    this.contentDoc = contentDocument;
-    this.urlBarInput = document.getElementById('urlbar-input');
-    this.urlMouseEvent = this.urlBarInput.addEventListener('blur', () => this.shortenURL());
-    gBrowser.tabContainer.addEventListener('TabSelect', (e) => this.shortenURL(e), false);
-    gBrowser.tabContainer.addEventListener("TabAttrModified", (e) => this.shortenURL(e), false);
-    gBrowser.tabContainer.addEventListener('TabOpen', (e) => this.shortenURL(e), false);
-    gBrowser.tabContainer.addEventListener('TabClose', (e) => this.shortenURL(e), false);
-    gBrowser.tabContainer.addEventListener('load', (e) => this.shortenURL(e), false);
-    gBrowser.tabContainer.addEventListener("DOMContentLoaded", (e) => this.shortenURL(e), false);
-  },
+    var urlBarInput = document.getElementById('urlbar-input');
+    var urlMouseEvent = urlBarInput.addEventListener('blur', () => shortenURL());
+    gBrowser.tabContainer.addEventListener('TabSelect', (e) => shortenURL(e), false);
+    gBrowser.tabContainer.addEventListener("TabAttrModified", (e) => shortenURL(e), false);
+    gBrowser.tabContainer.addEventListener('TabOpen', (e) => shortenURL(e), false);
+    gBrowser.tabContainer.addEventListener('TabClose', (e) => shortenURL(e), false);
+    gBrowser.tabContainer.addEventListener('load', (e) => shortenURL(e), false);
+    gBrowser.tabContainer.addEventListener("DOMContentLoaded", (e) => shortenURL(e), false);
 
-  destroy: function () {
-    delete UC.domainURL;
-  }
-}
+  } catch (err) { }
+
+})();
